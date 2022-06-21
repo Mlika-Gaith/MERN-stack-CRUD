e515cb7bd381a700b7fa1d37ef4cda6e16cad3f6
ghaith <mlika.gaith@gmail.com>
2022-06-21 22:36:31 +0100

    deploying app

--- server/server.js
+++ server/server.js
@@ -2,16 +2,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const route = require("./business.route");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URI || process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -21,7 +22,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/business", route);
-
+// for deployîng in heroko
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, function () {
   console.log("Server is running at : ", PORT);
 });
